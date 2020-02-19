1bb9e45ca219a0821501fe84d998df4d6b88140d
hanif05 <muamarhanif05@gmail.com>
2020-02-19 20:37:33 +0700

    add question factory and set database seeder

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Question;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        'title' => rtrim($faker->sentence(rand(5, 10)), "."), //rtrim for remove .(dot)
+        'body' => $faker->paragraphs(rand(3,7), true),
+        'answers' => rand(0, 10),
+        'votes' => rand(-4, 10),
+        'views' => rand(0, 10),
+    ];
+});
