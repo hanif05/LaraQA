eaeab7c3d8ef8013872cc965da21e73e6e96f421
hanif05 <muamarhanif05@gmail.com>
2020-02-23 23:00:35 +0700

    add factory answers & setting databaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Answer;
 use Illuminate\Database\Seeder;
 use App\User;
 use App\Question;
@@ -16,7 +17,12 @@ class DatabaseSeeder extends Seeder
         factory(User::class, 3)->create()->each(function($u){
             $u->questions()->saveMany(
                 factory(Question::class, rand(1, 5))->make()
-            );
+            )
+            ->each(function($q) {
+                $q->answers()->saveMany(
+                    factory(Answer::class, rand(1, 5))->make()
+                );
+            });
         });
     }
 }
