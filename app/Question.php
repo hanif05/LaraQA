1d71c5e9437f30af40064656d56c7c9ae64c4917
hanif05 <muamarhanif05@gmail.com>
2020-02-23 21:06:53 +0700

    add relation from Answer

--- app/Question.php
+++ app/Question.php
@@ -14,6 +14,10 @@ class Question extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 
     public function setTitleAttribute($value)
     {
