4d9357c14fd2c03859f62bd8594426ed8291b99b
hanif05 <muamarhanif05@gmail.com>
2020-02-27 09:53:55 +0700

    add delete answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -64,8 +64,12 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('delete', $answer);
+
+        $answer->delete();
+
+        return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been deleted!');
     }
 }
