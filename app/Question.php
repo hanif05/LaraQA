de1128272b26bbbf5dde65df04e4026ac4cbe851
hanif05 <muamarhanif05@gmail.com>
2020-02-29 23:13:25 +0700

    add relation Question User many to many

--- app/Question.php
+++ app/Question.php
@@ -19,6 +19,11 @@ class Question extends Model
         return $this->hasMany(Answer::class);
     }
 
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites');
+    }
+
     public function acceptBestAnswer(Answer $answer)
     {
         $this->best_answer_id = $answer->id;
