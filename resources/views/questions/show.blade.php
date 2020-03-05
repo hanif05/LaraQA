1e7a2673e3e4087d7b830c67b2e48757c1ec2648
hanif05 <muamarhanif05@gmail.com>
2020-03-05 14:50:31 +0700

    add form for add favaorite

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -24,11 +24,18 @@
                             <a href="#" title="This question not useful" class="vote-down off">
                                 <i class="fa fa-caret-down fa-3x"></i>
                             </a>
-                            <a href="#" class="favorite mt-2 favorited" title="Click to mark as favorite question (Click again to undo)">
+                            <a href="#" class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}" title="Click to mark as favorite question (Click again to undo)"
+                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit()">
                                 <i class="fa fa-star fa-2x"></i>
                                 <span class="favorites-count">{{ $question->favorites_count }}</span>
                                 {{-- $question->favorites_count same with $question->favorites()->count() --}}
                             </a>
+                            <form action="/questions/{{ $question->id }}/favorites" id="favorite-question-{{ $question->id }}" method="POST" style="display:none">
+                                @csrf
+                                @if ($question->is_favorited)
+                                    @method('DELETE')
+                                @endif
+                            </form>
                         </div>
                         <div class="media-body">
                             {!! $question->body_html !!}
