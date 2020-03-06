d32150bbae3ecbe6d4688f97aaa1994cb6b1acf9
hanif05 <muamarhanif05@gmail.com>
2020-03-06 22:08:56 +0700

    add php doc in User and Answer Model

--- app/Answer.php
+++ app/Answer.php
@@ -4,45 +4,93 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Answer
+ */
 class Answer extends Model
-{
+{    
+    /**
+     * Answer::fillable
+     *
+     * @var array
+     */
     protected $fillable = ['body', 'user_id'];
-
+    
+    /**
+     * Answer::user
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
+    
+    /**
+     * Answer::question
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function question()
     {
         return $this->belongsTo(Question::class);
     }
-
+    
+    /**
+     * Answer::votes
+     *
+     * @return Illuminate\Database\Eloquent\Relations\MorphToMany
+     */
     public function votes()
     {
         return $this->morphToMany(User::class, 'votable');
     }
-
+    
+    /**
+     * Answer::isBest
+     *
+     * @return bool
+     */
     public function isBest()
     {
         return $this->question->best_answer_id == $this->id;
     }
-
+    
+    /**
+     * Answer::getBodyHtmlAttribute
+     *
+     * @return string
+     */
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
     }
-
+    
+    /**
+     * Answer::getStatusAttribute
+     *
+     * @return string
+     */
     public function getStatusAttribute()
     {
         return  $this->isBest() ? 'votes-accepted' : '';
     }
-
+    
+    /**
+     * Answer::getIsBestAttribute
+     *
+     * @return bool
+     */
     public function getIsBestAttribute()
     {
         return $this->isBest();
     }
-
+    
+    /**
+     * Answer::boot
+     *
+     * @return void
+     */
     public static function boot()
     {
         parent::boot();
--- app/User.php
+++ app/User.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * Class User
+ */
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,39 +39,74 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
+    
+    /**
+     * User::questions
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function questions()
     {
         return $this->hasMany(Question::class);
     }
-
+    
+    /**
+     * User::answers
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function answers()
     {
         return $this->hasMany(Answer::class);
     }
-
+    
+    /**
+     * User::favorites
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function favorites()
     {
         return $this->belongsToMany(Question::class, 'favorites')->withTimestamps();
     }
-
+    
+    /**
+     * User::voteQuestions
+     *
+     * @return Illuminate\Database\Eloquent\Relations\MorphToMany
+     */
     public function voteQuestions()
     {
         return $this->morphedByMany(Question::class, 'votable');
     }
-
+    
+    /**
+     * User::voteAnswers
+     *
+     * @return Illuminate\Database\Eloquent\Relations\MorphToMany
+     */
     public function voteAnswers()
     {
         return $this->morphedByMany(Answers::class, 'votable');
     }
-
+    
+    /**
+     * User::getUrlAttribute
+     *
+     * @return string
+     */
     public function getUrlAttribute()
     {
         // return route('users.show', $this->id);
 
         return "#";
     }
-
+    
+    /**
+     * User::getAvatarAttribute
+     *
+     * @return string
+     */
     public function getAvatarAttribute()
     {
         $email = $this->email;
