3a0c300a260d887d1eddc8faaa41e3861aa232cd
hanif05 <muamarhanif05@gmail.com>
2020-03-06 22:16:27 +0700

    add php doc in FavoritesController

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -5,20 +5,40 @@ namespace App\Http\Controllers;
 use App\Question;
 use Illuminate\Http\Request;
 
+/**
+ * Class FavoritesController
+ */
 class FavoritesController extends Controller
-{
+{    
+    /**
+     * this is middleware auth
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
-
+    
+    /**
+     * Store a newly created favorite question in storage.
+     *
+     * @param  App\Question $question
+     * @return Illuminate\Http\RedirectResponse
+     */
     public function store(Question $question)
     {
         $question->favorites()->attach(auth()->id());
 
         return back();
     }
-
+    
+    /**
+     * Remove a favorited question from storage
+     *
+     * @param  App\Question $question
+     * @return Illuminate\Http\RedirectResponse
+     */
     public function destroy(Question $question)
     {
         $question->favorites()->detach(auth()->id());
