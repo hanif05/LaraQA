11cd19cb437ac803e1cfda3547c0d9db19561390
hanif05 <muamarhanif05@gmail.com>
2020-03-21 11:25:54 +0700

    add UserInfo vue

---
+++ resources/js/components/UserInfo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <span class="text-muted">{{ postDate }}</span>
+        <div class="media mt-2">
+            <a :href="users.url" class="pr-2">
+                <img :src="users.avatar" alt="" srcset="">
+            </a>
+            <div class="media-body mt-1">
+                <a :href="users.url">{{ users.name }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['model', 'label'],
+
+    computed: {
+        postDate() {
+            return this.label + " " + this.model.created_date;
+        },
+    },
+    data () {
+        return {
+            users: this.model.user,
+        }
+    },
+}
+</script>
